75ef24ee41fced30eee0cb1aeaeb3fbb1a20e1fc
kajela <titoyut56@gmail.com>
2022-06-07 01:25:40 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] city ={"melborne","sydney","queensland","adelaide"};
+                for (int i =0; i < city.length;i++) {
+                    System.out.println(city[i]);
+                }
+                //for - each loop
+                int[] marks ={4,5,6,7,89,};
+                for (int t: marks) {
+                    System.out.println(t);
+                }
+                //multidirectional array.
+        String[][] objects={{"shisha","coal","flavor"},{"port","bow","flask"}};
+                String y= objects[1][2];
+                System.out.println(y);
+                int[][] gpa ={{21,23,56,78,98},{5,7,9,2,1}};
+                for (int i =0; i < gpa.length;i++){
+                    for (int k=0; k <gpa[i].length;k++){
+                        System.out.println(gpa[i][k]);
+                    }
+                }
     }
 }
